ef4a778d90a4cf790508fb41a021817c37fcdd0f
Sinu <sin00528@gmail.com>
2021-07-01 17:03:47 +0900

    0701

---
+++ accountapp/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from accountapp.views import hello_world
+
+app_name = 'accountapp'
+
+urlpatterns = [
+    # name = '___'는 라우트의 이름을 지정
+    path('hello_world/', hello_world, name='hello_world'),
+]
