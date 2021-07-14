2765dd0208f384df21b964d2929376f51ba68c40
Sinu <sin00528@gmail.com>
2021-07-14 11:36:57 +0900

    0714 commit 1

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = 'accountapp'
 
 urlpatterns = [
     # name = '___'는 라우트의 이름을 지정
     path('hello_world/', hello_world, name='hello_world'),
+    path('create/', AccountCreateView.as_view(), name='create')
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,12 @@
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
+
 from accountapp.models import HelloWorld
 
 
@@ -9,14 +14,21 @@ def hello_world(request):
     if request.method == "POST":
 
         temp = request.POST.get('input')
-        
+
         new_data = HelloWorld()
         new_data.text = temp
-        # DB에 저장
         new_data.save()
 
-        return render(request, 'accountapp/hello_world.html',
-                      context={'new_data': new_data})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+
     else:
+        data_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': 'GET METHOD'})
+                      context={'data_list': data_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
